e0d2a13c3ad1ee66281099a13a74425c39976344
Aniket <anikettikudave187@gmail.com>
2025-03-06 16:01:51 +0530

    updated testCase class

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,5 +1,20 @@
 package com.driver.test;
 
 public class TestCases {
+    public TestCases() { /* compiled code */ }
 
+    @org.junit.jupiter.api.Test
+    public final void testVehicle() { /* compiled code */ }
+
+    @org.junit.jupiter.api.Test
+    public final void testCar1() { /* compiled code */ }
+
+    @org.junit.jupiter.api.Test
+    public final void testCar2() { /* compiled code */ }
+
+    @org.junit.jupiter.api.Test
+    public final void testF11() { /* compiled code */ }
+
+    @org.junit.jupiter.api.Test
+    public final void testF12() { /* compiled code */ }
 }
